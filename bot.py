f17e71ba2fa56ddc9fb96d9fa96f645bf1397a16
David11253 <minerusiapiter58@gmail.com>
2025-02-07 21:54:56 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,54 +1,30 @@
 from telegram import Bot
 from telegram.ext import CommandHandler, Updater
-
+import os
 from flask import Flask, request
 
 TOKEN = '7705095327:AAGTdo2oXWMACVl8cufB-gYzDNzD4UxTUiU'  # Твой токен
-bot = telebot.TeleBot(TOKEN)
+bot = Bot(TOKEN)
 
-# Flask-сервер для обработки данных формы
 app = Flask(__name__)
 
-# ID твоей группы в Telegram
-GROUP_ID = '-1002428849357'
+# Команда /start для бота
+def start(update, context):
+    update.message.reply_text("Привет, я твой бот!")
 
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    if request.method == 'POST':
-        data = request.form
-        nickname = data['nickname']
-        device = data['device']
-        server = data['server']
-        redstone = data['redstone']
-        playstyle = data['playstyle']
-        playtime = data['playtime']
-        study = data['study']
-        teammates = data['teammates']
-        country = data['country']
-        age = data['age']
-        internet = data['internet']
-        contact = data['contact']
+# Инициализация Updater и Dispatcher
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-        message = f"""
-        Новая заявка на вступление:
-        Никнейм: {nickname}
-        ПК или Мб: {device}
-        На каком сервере играете: {server}
-        Редстоун: {redstone}/10
-        ПвП или ПвЕ: {playstyle}
-        Часы игры в день: {playtime}
-        Учеба: {study}
-        Тимейты: {teammates}
-        Страна: {country}
-        Возраст: {age}
-        Интернет: {internet}/10
-        Контакт: {contact}
-        """
-        
-        bot.send_message(GROUP_ID, message)
-        bot.send_poll(GROUP_ID, "Голосование: принять заявку?", options=["Принять", "Отказать"], is_anonymous=True)
+# Добавление обработчика команды
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-        return "Заявка отправлена!"
+# Flask-сервер
+@app.route('/')
+def index():
+    return "Hello, world!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    updater.start_polling()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
